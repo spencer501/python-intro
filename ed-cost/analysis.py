85a01cecb924a1302b31fe2771d44d7e9715df47
Spencer <spencer.p.816@gmail.com>
2017-01-11 15:04:53 -0800

    Work from lab

--- ed-cost/analysis.py
+++ ed-cost/analysis.py
@@ -5,8 +5,8 @@
 # import pandas and plotly. You may want to comment out the plotly import until
 # you get to that part because the code runs much slower with it
 import pandas
-# from plotly.offline import plot
-# import plotly.graph_objs as go
+from plotly.offline import plot
+import plotly.graph_objs as go
 
 ##################
 # Python Warm-up #
@@ -17,6 +17,14 @@ import pandas
 # letter. Print the result of calling your function on n_arr.
 n_arr = ["Mike", "Linus", "Grace"]
 
+def hasLetter(array, let):
+    new_list = []
+    for element in array:
+        if (let in element):
+            new_list.append(element)
+    return new_list
+    
+print (hasLetter(n_arr, 'i'))
 
 # BONUS: Do it without a loop
 
@@ -31,19 +39,29 @@ ed_data = pandas.read_csv("data/cost-data.csv", encoding="iso-8859-1")
 # Print the number of rows and columns in the data in the format
 # rows=#, cols=#
 shape = ed_data.shape
-print("rows=%d, cols=%d" % shape)
-print()
+print ("rows=%d, cols=%d" % shape)
+print ()
 
 # Change the following column names in your data frame
 # 2012-13 Tuition and fees  -> tuition.2012
 # 2014-15 Tuition and fees  -> tuition.2014
-# Sectior name              -> sector
+# Sector name               -> sector
 # Name of institution       -> instituion
+ed_data = ed_data.rename(columns = {'2012-13 Tuition and fees':'tuition.2012',
+                                    '2014-15 Tuition and fees':'tuition.2014',
+                                    'Sector name':'sector',
+                                    'Name of institution':'institution'})
 
 
 # How many UNIQUE institutions are there? What data structure could you
 # leverage?
+print ("there are %d unique institutions" % 
+       len(ed_data['institution'].unique()))
 
+# or
+
+ninst = len(set(ed_data['institution']))
+print (ninst, "unique institutions")
 
 # What types of schools are there? How many of each type are there?
 # Hint: You can do this using pandas or stock python
@@ -64,8 +82,8 @@ wa_data["tuition_rank"] = wa_data["tuition.2014"].rank(numeric_only=True)
 
 rank = wa_data[wa_data.institution ==
                "University of Washington-Seattle Campus"].tuition_rank.iloc[0]
-print("UW 2014 Tuition Rank: %d" % rank)
-print()
+print ("UW 2014 Tuition Rank: %d" % rank)
+print ()
 
 # Which *sector* had the largest average change in tuition?
 
